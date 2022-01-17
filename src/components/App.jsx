d2b4a3e05881500497d6e8e732c90e0bb04b5657
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-01-17 10:36:53 +0100

    v0.2
    Add Footer component.
    Add animations.
    Edit CSS.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import NavButton from "./NavButton";
+import Footer from "./Footer";
+
+import { gsap } from "gsap";
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [pictures, setPictures] = useState([]);
   const [currentPic, setCurrentPic] = useState(0);
+  const [direction, setDirection] = useState("");
 
+  const pictureRef = useRef();
+
+  // Only runs after first render
   useEffect(() => {
     fetch("https://silver-le-maine-coon.herokuapp.com/api/photos")
       .then((res) => res.json())
@@ -21,14 +28,34 @@ function App() {
           setError(error);
         }
       );
+    // .then(() => {
+    //   scaleInPicture();
+    // });
   }, []);
+
+  // Runs after first render every time `currentPic` changes if isLoaded.
+  // useEffect(() => {
+  //   if (isLoaded) {
+  //     scaleInPicture();
+  //   }
+  // }, [currentPic]);
+
+  function scaleInPicture() {
+    // gsap.from(pictureRef.current, { scale: 0 });
+
+    direction === "left"
+      ? gsap.from(pictureRef.current, { x: "-50%", autoAlpha: 0 })
+      : gsap.from(pictureRef.current, { x: "50%", autoAlpha: 0 });
+  }
 
   function getPrevPicture() {
     setCurrentPic((previousValue) => previousValue - 1);
+    setDirection("left");
   }
 
   function getNextPicture() {
     setCurrentPic((previousValue) => previousValue + 1);
+    setDirection("right");
   }
 
   if (error) {
@@ -37,18 +64,25 @@ function App() {
     return <div>Chargement...</div>;
   } else {
     return (
-      <div>
-        <div className="picture">
-          <img src={pictures[currentPic]} />
-          <div className="nav-buttons">
-            {currentPic != 0 && (
-              <NavButton direction="left" onPictureChange={getPrevPicture} />
-            )}
-            {currentPic != pictures.length - 1 && (
-              <NavButton direction="right" onPictureChange={getNextPicture} />
-            )}
+      <div className="page">
+        <div className="content">
+          <div className="picture">
+            <img
+              src={pictures[currentPic]}
+              ref={pictureRef}
+              onLoad={() => scaleInPicture()}
+            />
+            <div className="nav-buttons">
+              {currentPic != 0 && (
+                <NavButton direction="left" onPictureChange={getPrevPicture} />
+              )}
+              {currentPic != pictures.length - 1 && (
+                <NavButton direction="right" onPictureChange={getNextPicture} />
+              )}
+            </div>
           </div>
         </div>
+        <Footer quote="quote" author="author" />
       </div>
     );
   }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer(props) {
+  return (
+    <div className="footer">
+      <div className="container">
+        <p>
+          ❝ <i>{props.quote}</i> ❞ - {props.author}
+        </p>
+        © {new Date().getFullYear()},{" "}
+        <a href="https://github.com/Julien-B-py">
+          Julien BEAUJOIN <i className="fab fa-github"></i>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
